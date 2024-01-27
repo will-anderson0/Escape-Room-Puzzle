b05273f59734455d1b10fe2e19f24216d2138138
William Anderson <wanderson.wa252@gmail.com>
2024-01-26 21:09:56 -0500

    final edits

--- src/components/pages/DataEnter.tsx
+++ src/components/pages/DataEnter.tsx
@@ -1,7 +1,7 @@
-import { Grid, TextField, Typography } from "@mui/material";
+import { Grid, TextField, Typography, Container } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ikeaLogo from "./ikeaLogo.jpg";
+import ikeaLogo from "./ikeaIcon.jpg";
 
 function DataEnter() {
   const [code, setCode] = useState("");
@@ -19,43 +19,57 @@ function DataEnter() {
   };
 
   return (
-    <Grid
-      container
-      direction="column"
+    <Container
       sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
         backgroundColor: "#000000",
         color: "#ffffff",
+        minHeight: "100vh",
+        minWidth: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "20px",
       }}
     >
-      <img
-        src={ikeaLogo}
-        style={{ width: "50%", height: "auto" }}
-        alt="Ikea Logo"
-      />
-      <Grid item xs={12}>
-        <Typography sx={{ fontSize: "36px" }}>Enter 4 digit code</Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <TextField
-          sx={{
-            width: "250px",
-            borderRadius: "4px",
-            fontSize: "16px",
-            padding: "12px",
-            backgroundColor: "#ffffff",
-          }}
-          id="standard-basic"
-          label="Enter your code here"
-          variant="standard"
-          type="text"
-          value={code}
-          onChange={handleCodeChange}
+      <Grid
+        container
+        direction="column"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#000000",
+          color: "#ffffff",
+        }}
+      >
+        <img
+          src={ikeaLogo}
+          style={{ width: "50%", height: "auto" }}
+          alt="Ikea Logo"
         />
+        <Grid item xs={12}>
+          <Typography sx={{ fontSize: "36px" }}>Enter 4 digit code</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            sx={{
+              width: "250px",
+              borderRadius: "4px",
+              fontSize: "16px",
+              padding: "12px",
+              backgroundColor: "#ffffff",
+            }}
+            id="standard-basic"
+            label="Enter your code here"
+            variant="standard"
+            type="text"
+            value={code}
+            onChange={handleCodeChange}
+          />
+        </Grid>
       </Grid>
-    </Grid>
+    </Container>
   );
 }
 
--- src/components/pages/Riddle.tsx
+++ src/components/pages/Riddle.tsx
@@ -1,12 +1,28 @@
+import { Typography, Container } from "@mui/material";
+
 function Riddle() {
   return (
-    <div>
-      Your items have been verified for checkout! The current wait time for
-      checkout is 25 minutes. You don’t know me but I can help you get to the
-      front of the line right away! Come get me in the backpack near the front
-      of the store. They locked me away to keep my secrets hidden but my
-      passcode is 3245.
-    </div>
+    <Container
+      sx={{
+        backgroundColor: "#000000",
+        color: "#ffffff",
+        minHeight: "100vh",
+        minWidth: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "20px",
+      }}
+    >
+      <Typography variant="body1" width="75%" fontSize="50px">
+        Your items have been verified for checkout! The current wait time for
+        checkout is 25 minutes. You don’t know me but I can help you get to the
+        front of the line right away! Come get me in the backpack near the front
+        of the store. They locked me away to keep my secrets hidden but my
+        passcode is 3245.
+      </Typography>
+    </Container>
   );
 }
 
